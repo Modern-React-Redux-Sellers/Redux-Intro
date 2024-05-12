b40c685558df761d3a31c555b7374fb871a48f5d
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2024-05-11 19:12:23 -0500

    refactored reset to utilize createAction hook

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
-import {configureStore, createSlice} from "@reduxjs/toolkit";
+import {configureStore, createSlice, createAction} from "@reduxjs/toolkit";
+
+export const reset = createAction("app/reset");
 
 
 const moviesSlice = createSlice({
@@ -11,17 +13,19 @@ const moviesSlice = createSlice({
         removeMovie(state, action){
             const index = state.indexOf(action.payload);
             state.splice(index, 1);
-        },
-        reset(state, action){
-            return []; //cannot do state = [];
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(reset, (state, action)=> {
+            return [];
+        })
     }
 })
 
 const songsSlice = createSlice({
     name: 'song',
     initialState: [], //defines initial state
-    reducers: {     //mini-reducers
+    reducers: {     //mini-reducers/actions
         // 'song' from name: + '/' + 'addSong' = 'song/addSong' becomes function call from .dispatch 'type' parameter
         //state is only state managed by this reducer, not overall state
         addSong(state, action){
@@ -34,8 +38,8 @@ const songsSlice = createSlice({
         }
     },
     extraReducers: (builder) => {
-        //watches for movie/reset action to reset state of song array
-        builder.addCase(moviesSlice.actions.reset, (state, action) => {
+        //watches for reset action to reset state of song array
+        builder.addCase(reset, (state, action) => {
             return []; //sets state to empty
         })
     }
@@ -65,4 +69,4 @@ export {store};
 
 //export action creator -> SongPlaylist component
 export const {addSong, removeSong} = songsSlice.actions;
-export const {addMovie, removeMovie, reset} = moviesSlice.actions;
+export const {addMovie, removeMovie} = moviesSlice.actions;
